4a9b4cf816f5fa8b3fafb608961bfe7ac5bf6fd3
tomin <ftouming@gmail.com>
2025-04-30 15:50:34 +0800

    format

--- utils.py
+++ utils.py
@@ -2,6 +2,6 @@ import tomllib
 from pathlib import Path
 def load_config():
     toml_file = Path('config.toml')
-    with toml_file.open('r') as f: 
+    with toml_file.open('rb') as f: 
         cfg = tomllib.load(f)
     return cfg
